ef91dbc9002ca9f8de277a8a4c22f4812545a2c5
Theresemariej <123539601+Theresemariej@users.noreply.github.com>
2024-09-25 13:09:45 +0200

    Update Mediatheque.java

--- src/main/java/mediatheque/Mediatheque.java
+++ src/main/java/mediatheque/Mediatheque.java
@@ -6,7 +6,24 @@ import java.util.List;
 public class Mediatheque{
 	private final List<Item> items = new LinkedList<>();
 
-	public void addItem(Item i) {
-		items.add(i);
-	}
+	  public void printCatalog() {
+        for (Item i : items)
+            i.print();
+    }
+
+    public void printOnlyBook() {
+        printOnlyBook printBookVisitor = new printOnlyBook();
+        for (Item item : items) {
+            item.accept(printBookVisitor);
+        }
+    }
+
+
+    public void printOnlyCD() {
+        printOnlyCD printCDVisitor = new printOnlyCD();
+        for (Item item : items) {
+            item.accept(printCDVisitor);
+        }
+    }
+
 }
